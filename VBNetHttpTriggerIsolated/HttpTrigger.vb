4ea04d57c571b3662f1a6acb6322272aa8e3197d
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2023-02-15 13:57:07 +1300

    First cut of isolated httptrigger

---
+++ VBNetHttpTriggerIsolated/HttpTrigger.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Net
+Imports Microsoft.Azure.Functions.Worker
+Imports Microsoft.Azure.Functions.Worker.Http
+Imports Microsoft.Extensions.Logging
+
+Namespace VBNetHttpTriggerIsolated
+    Public Class Function1
+        Private ReadOnly _logger As ILogger
+
+        Public Sub New(ByVal loggerFactory As ILoggerFactory)
+            _logger = loggerFactory.CreateLogger(Of Function1)()
+        End Sub
+
+        <[Function]("Notifications")>
+        Public Function Run(
+        <HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")> ByVal req As HttpRequestData) As HttpResponseData
+            _logger.LogInformation("C# HTTP trigger function processed a request.")
+
+            Dim response = req.CreateResponse(HttpStatusCode.OK)
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8")
+
+            response.WriteString("Welcome to Azure Functions!")
+
+            Return response
+        End Function
+    End Class
+End Namespace
---
+++ VBNetHttpTriggerIsolated/Program.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.Azure.Functions.Worker
+Imports Microsoft.Extensions.Hosting
+
+Namespace VBNetHttpTriggerIsolated
+    Friend Class Program
+        Public Shared Sub Main(ByVal args As String())
+            Call FunctionsDebugger.Enable()
+
+            Dim host = New HostBuilder().ConfigureFunctionsWorkerDefaults().Build()
+
+            host.Run()
+        End Sub
+    End Class
+End Namespace
