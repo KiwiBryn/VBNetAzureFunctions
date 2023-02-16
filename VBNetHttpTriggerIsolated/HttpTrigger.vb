6a329fa6acc33123a042b473801c2c2ea6cdf62c
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2023-02-16 16:31:32 +1300

    Added C# notices, renamed class, added finished logging code

--- VBNetHttpTriggerIsolated/HttpTrigger.vb
+++ VBNetHttpTriggerIsolated/HttpTrigger.vb
@@ -1,25 +1,44 @@
-﻿Imports System.Net
+﻿'---------------------------------------------------------------------------------
+' Copyright (c) February 2023, devMobile Software
+'
+' Licensed under the Apache License, Version 2.0 (the "License");
+' you may Not use this file except in compliance with the License.
+' You may obtain a copy of the License at
+'
+'     http://www.apache.org/licenses/LICENSE-2.0
+'
+' Unless required by applicable law Or agreed to in writing, software
+' distributed under the License Is distributed on an "AS IS" BASIS,
+' WITHOUT WARRANTIES Or CONDITIONS OF ANY KIND, either express Or implied.
+' See the License for the specific language governing permissions And
+' limitations under the License.
+'
+'---------------------------------------------------------------------------------
+Imports System.Net
+Imports System.Threading
 Imports Microsoft.Azure.Functions.Worker
 Imports Microsoft.Azure.Functions.Worker.Http
 Imports Microsoft.Extensions.Logging
+Imports Microsoft.VisualBasic.Logging
 
 Namespace VBNetHttpTriggerIsolated
-    Public Class Function1
+    Public Class HttpTrigger
+        Private Shared executionCount As Int32
         Private ReadOnly _logger As ILogger
 
         Public Sub New(ByVal loggerFactory As ILoggerFactory)
-            _logger = loggerFactory.CreateLogger(Of Function1)()
+            _logger = loggerFactory.CreateLogger(Of HttpTrigger)()
         End Sub
 
         <[Function]("Notifications")>
         Public Function Run(
         <HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")> ByVal req As HttpRequestData) As HttpResponseData
-            _logger.LogInformation("C# HTTP trigger function processed a request.")
+            Interlocked.Increment(executionCount)
+
+            _logger.LogInformation("VB.Net NET 4.8 Isolated HTTP trigger Execution count:{executionCount} Method:{req.Method}", executionCount, req.Method)
 
             Dim response = req.CreateResponse(HttpStatusCode.OK)
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8")
-
-            response.WriteString("Welcome to Azure Functions!")
 
             Return response
         End Function
--- VBNetHttpTriggerIsolated/Program.vb
+++ VBNetHttpTriggerIsolated/Program.vb
@@ -1,4 +1,20 @@
-﻿Imports Microsoft.Azure.Functions.Worker
+﻿'---------------------------------------------------------------------------------
+' Copyright (c) February 2023, devMobile Software
+'
+' Licensed under the Apache License, Version 2.0 (the "License");
+' you may Not use this file except in compliance with the License.
+' You may obtain a copy of the License at
+'
+'     http://www.apache.org/licenses/LICENSE-2.0
+'
+' Unless required by applicable law Or agreed to in writing, software
+' distributed under the License Is distributed on an "AS IS" BASIS,
+' WITHOUT WARRANTIES Or CONDITIONS OF ANY KIND, either express Or implied.
+' See the License for the specific language governing permissions And
+' limitations under the License.
+'
+'---------------------------------------------------------------------------------
+Imports Microsoft.Azure.Functions.Worker
 Imports Microsoft.Extensions.Hosting
 
 Namespace VBNetHttpTriggerIsolated
